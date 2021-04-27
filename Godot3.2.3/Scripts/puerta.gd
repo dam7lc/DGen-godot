7571c9eb424bc45a6bccef460dcb7aacc837308c
Mrhama9919 <abrahammoralesaviles@outlook.com>
2021-04-27 18:15:49 -0500

    puerta en cada habitacion

---
+++ Godot3.2.3/Scripts/puerta.gd
@@ -0,0 +1,28 @@
+extends StaticBody
+var area
+var anim_player
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	area = get_child(3)
+	anim_player = get_node("AnimationPlayer")
+	print(area)
+	area.connect("body_entered", self, "jugador_entro")
+	area.connect("body_exited", self, "jugador_sale")
+	anim_player.stop(true)
+func jugador_entro(body):
+	if body.get_script() == load("res://Scripts/Player.gd"):
+		anim_player.play("Abrir")
+	
+func jugador_sale(body):
+	if body.get_script() == load("res://Scripts/Player.gd"):
+		anim_player.play("Cerrar")
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
